f4f65d5f6d595aa0c4e05af4021aacce61bd6cc7
Rahul <rahulpatel.spaceo@gmail.com>
2018-10-24 23:24:22 +0530

    Initial release

--- ConnectDot/ViewController.swift
+++ ConnectDot/ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let htmlFile = Bundle.main.path(forResource: "index", ofType: "html")
+        let html = try? String(contentsOfFile: htmlFile!, encoding: String.Encoding.utf8)
+        webView.loadHTMLString(html!, baseURL: nil)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
